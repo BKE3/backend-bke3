dc3d260610d3baca860888f3fd7dc07158b171db
Beth <bethmelmtv@gmail.com>
2022-06-29 16:04:33 -0700

    added createBird function to fetch utils file

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,17 +1,22 @@
 export async function getAllBirds() {
-  const rawResponse = await fetch('http://localhost:7890/api/v1/birds');
+  const rawResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/birds`);
   const data = await rawResponse.json();
   return data;
 }
 
 export async function getBirdById(id) {
-  const rawResponse = await fetch(`http://localhost:7890/api/v1/birds/${id}`);
+  const rawResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/birds/${id}`);
   const data = await rawResponse.json();
   return data;
 }
 
-export async function createBird() {
-  const rawResponse = await fetch(`http://localhost:7890/api/v1/birds/create`);
+export async function createBird(bird) {
+  const rawResponse = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/birds/create`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(bird),
+  });
   const data = await rawResponse.json();
   return data;
 }
+//method, headers, body
