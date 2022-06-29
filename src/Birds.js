8e44091ee05af37db584e48d18e25a55c92a21e7
Beth <bethmelmtv@gmail.com>
2022-06-29 11:14:37 -0700

    worked on birds.js

--- src/Birds.js
+++ src/Birds.js
@@ -1,14 +1,23 @@
 import React from 'react';
 //call the fetch utils function
 import { useState, useEffect } from 'react';
+import getAllBirds from fetch.js;
 
 export default function Birds() {
+  const [birds, setBirds] = useState([]);
   //use state that has all the birds
   //useEffect on load which will caall the fetch utils that will grab all the birds
 
+  useEffect(() => {
+   const birdData = await getAllBirds();
+   setBirds(birdData);
+  }, []);
+
   return (
-    <div>Birds</div>
-    //map ove all the birds and grab the birds general details
-    //clickable and will reroute to the birds detail page
+    <div> Birds
+      {birds.map((bird, i) => (
+        <BirdCard key = {bird + i}{...bird} /> 
+      ))}
+    </div>
   );
 }
