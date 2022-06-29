abcc0522d60c8379c7a698cfa4cd6577a721322c
Beth <bethmelmtv@gmail.com>
2022-06-29 11:39:17 -0700

    worked on editing birds.js

--- src/Birds.js
+++ src/Birds.js
@@ -1,7 +1,8 @@
 import React from 'react';
 //call the fetch utils function
 import { useState, useEffect } from 'react';
-import getAllBirds from fetch.js;
+import { getAllBirds } from './services/fetch-utils';
+import { BirdCard } from ' BirdCard.js';
 
 export default function Birds() {
   const [birds, setBirds] = useState([]);
@@ -9,14 +10,16 @@ export default function Birds() {
   //useEffect on load which will caall the fetch utils that will grab all the birds
 
   useEffect(() => {
-   const birdData = await getAllBirds();
-   setBirds(birdData);
+    const birdData = getAllBirds();
+    setBirds(birdData);
   }, []);
 
   return (
-    <div> Birds
+    <div>
+      {' '}
+      Birds
       {birds.map((bird, i) => (
-        <BirdCard key = {bird + i}{...bird} /> 
+        <BirdCard key={bird + i} {...bird} />
       ))}
     </div>
   );
