cc78f4565751b6304d3fd4af0104547f65852f9b
Beth <bethmelmtv@gmail.com>
2022-06-29 11:01:54 -0700

    worked on app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { Detail } from './Detail';
+import { Birds } from './Birds';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/birds"> All Birds </Link>
+          </li>
+        </ul>
+      </div>
+
+      <Switch>
+        <Route exact path="/birds">
+          <Birds />
+        </Route>
+
+        <Route exact path="/birds/:id">
+          <Detail />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Birds.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//call the fetch utils function
+import { useState, useEffect } from 'react';
+
+export default function Birds() {
+  //use state that has all the birds
+  //useEffect on load which will caall the fetch utils that will grab all the birds
+
+  return (
+    <div>Birds</div>
+    //map ove all the birds and grab the birds general details
+    //clickable and will reroute to the birds detail page
+  );
+}
